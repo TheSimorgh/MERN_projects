40e41447471cf23ebb1fe2677699c3c4ae3be43d
TheSimorgh <TheSimurgh30@proton.me>
2023-07-08 14:17:47 +0500

    +config axios + tmdb config

---
+++ m_01/server/src/tmdb/tmdb.config.js
@@ -0,0 +1,18 @@
+
+const baseUrl = "https://api.themoviedb.org/3/";
+const key ="185ee3a9083112f300c90a61dd26cac0"
+// const baseUrl = process.env.TMDB_BASE_URL;
+// const key = process.env.TMDB_KEY;
+// https://api.themoviedb.org/3/
+// 185ee3a9083112f300c90a61dd26cac0
+// TMDB_BASE_URL=https://api.themoviedb.org/3/
+// TMDB_KEY=185ee3a9083112f300c90a61dd26cac0
+
+
+const getUrl = (endpoint, params) => {
+  const qs = new URLSearchParams(params);
+
+  return `${process.env.TMDB_BASE_URL}${endpoint}?api_key=${process.env.TMDB_KEY}&${qs}`;
+};
+
+export default { getUrl };
