e0f0e56dffa784961bedfe95b02a5dfba0af95e1
TheSimorgh <TheSimurgh30@proton.me>
2023-07-09 09:17:10 +0500

    getDetail ctrl

--- m_01/server/src/controllers/media.ctrl.js
+++ m_01/server/src/controllers/media.ctrl.js
@@ -34,7 +34,48 @@ const search = async (req, res) => {
   try {
     const { mediaType } = req.params;
     const { query, page } = req.query;
-    const response=await tmdbApi.mediaSearch({query,page,mediaType:mediaType==="people"?"person":mediaType})
+    const response = await tmdbApi.mediaSearch({
+      query,
+      page,
+      mediaType: mediaType === "people" ? "person" : mediaType,
+    });
+    return responseHandler.ok(res, response);
+  } catch (error) {
+    responseHandler.error(res);
+  }
+};
+const getDetail = async (req, res) => {
+  try {
+    const { mediaType, mediaId } = req.params;
+    const params = { mediaType, mediaId };
+    const media = await tmdbApi.mediaDetail(params)
+    console.log(params);
+    console.log("media");
+    console.log(media);
+    media.credits=await tmdbApi.mediaCredits(params)
+    const videos = await tmdbApi.mediaVideos(params);
+    media.videos = videos;
+    console.log("videos");
+    console.log(videos);
+    const recommend = await tmdbApi.mediaRecommend(params);
+    console.log("recommend");
+    console.log(recommend);
+    media.recommend = recommend.results;
+    media.images = await tmdbApi.mediaImages(params);
+    const tokenDecoded = tokenMiddleware.tokenDecode(req);
+
+    if (tokenDecoded) {
+      const user = await userModel.findById(tokenDecoded.data);
+
+      if (user) {
+        const isFavorite = await favoriteModel.findOne({ user: user.id, mediaId });
+        media.isFavorite = isFavorite !== null;
+      }
+    }
+
+    media.reviews = await reviewModel.find({ mediaId }).populate("user").sort("-createdAt");
+
+
     return responseHandler.ok(res, response);
   } catch (error) {
     responseHandler.error(res);
