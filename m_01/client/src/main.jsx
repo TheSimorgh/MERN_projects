99cd8706c489f09066065245dde22999dc8ea2b9
TheSimorgh <TheSimurgh30@proton.me>
2023-07-10 13:44:20 +0500

    +themeSlice

--- m_01/client/src/main.jsx
+++ m_01/client/src/main.jsx
@@ -4,14 +4,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import store from "./redux/store.jsx";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <Provider store={store}>
+  <Provider store={store}>
     <BrowserRouter>
       {/* <React.StrictMode> */}
         <App />
       {/* </React.StrictMode> */}
     </BrowserRouter>
-  // </Provider>
+  </Provider>
 );
