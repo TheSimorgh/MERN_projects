9edc92e95e91bc89f7749ff9193d3571e73c559e
TheSimorgh <TheSimurgh30@proton.me>
2023-07-10 14:40:34 +0500

    + appStateSlice

--- m_01/client/src/redux/store.jsx
+++ m_01/client/src/redux/store.jsx
@@ -1,11 +1,13 @@
 import {configureStore} from "@reduxjs/toolkit"
 import userSlice from "./features/userSlice"
 import themeModeSlice from "./features/themeModeSlice"
+import appStateSlice from "./features/appStateSlice"
 
 const store=configureStore({
     reducer:{
         user:userSlice,
         themeMode:themeModeSlice,
+        appState:appStateSlice,
     }
 })
 
