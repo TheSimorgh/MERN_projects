b7b9a3e8014909a05ebc75403bee80d0575b68a2
TheSimorgh <TheSimurgh30@proton.me>
2023-07-10 20:53:16 +0500

    +ProtectedPage

--- m_01/client/src/routes/MainRoute.jsx
+++ m_01/client/src/routes/MainRoute.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
 import { Routes, Route } from "react-router-dom";
 
 const MainRoute = () => {
   return (
     <>
-    <Routes></Routes>
+    <Routes>
+      <Route  />
+    </Routes>
     </>
       
   
