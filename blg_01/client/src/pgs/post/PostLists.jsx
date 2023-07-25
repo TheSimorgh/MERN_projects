e5e79b50140af82d837b8660432f0d1437b3a766
TheSimorgh <TheSimurgh30@proton.me>
2023-07-25 22:26:31 +0500

    Refactoring

--- blg_01/client/src/pgs/post/PostLists.jsx
+++ blg_01/client/src/pgs/post/PostLists.jsx
@@ -7,7 +7,7 @@ import { useDispatch, useSelector } from "react-redux";
 
 import { Link, useNavigate } from "react-router-dom";
 import truncatePost from "../../utils/truncatePost";
-import { get_private_posts, get_public_posts } from "../../redux/features/postSlice";
+import { get_private_posts } from "../../redux/features/postSlice";
 import { get_categories } from "../../redux/features/categorySlice";
 import { LoadingCmp } from "../../cmps";
 
@@ -34,7 +34,7 @@ const PostLists = () => {
   //dispatch fetching posts
   useEffect(() => {
     dispatch(get_private_posts({ page, limit: 4, searchTerm, category }));
-    dispatch(get_public_posts());
+    dispatch(get_categories());
   }, [dispatch, page, searchTerm, category]);
 
   const { categories } = useSelector((state) => state?.categories);
